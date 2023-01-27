72147bdfa554bac17f0cea602fd5b70afeafd9dc
José Rodrigues <jose.2000.felipe@gmail.com>
2023-01-27 15:59:42 -0300

    Operadores Relacionais

--- src/operadorternario/OperadorTernario.java
+++ src/operadorternario/OperadorTernario.java
@@ -15,6 +15,22 @@ public class OperadorTernario {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int n1, n2, r;
+        n1 = 14;
+        n2 = 18;
+        r = (n1>n2)?n1:n2;
+        // r = (n1>n2)?n1+n2:n1-n2;
+        System.out.println(r);
+        // Operadores Relacionais
+        /*
+        > Maior que 5 > 2 = true
+        < Menor que 4 < 1 = false
+        >= Maior ou igual a  8 >= 3 true
+        <= Menor ou igual a 6 <=6 = true
+        == igual 9 == 8 = false
+        != Diferente de 4 != 5 = true
+        */
     }
     
 }
